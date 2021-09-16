59d834f57a686870db3b2c6e018b57c3a8a85a63
Федя <Saheded@mail.ru>
2021-09-17 05:31:22 +0700

    Step 3. EXPAND EXPAND EXPAND

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 function myFunction() {
-    /* Get the text field */
     var copyText = document.getElementById("logo");
-
-    /* Select the text field */
-    copyText.select();
-
-    /* Copy the text inside the text field */
-    document.execCommand("copy");
-
-    /* Alert the copied text */
-    alert("Copied the text: " + copyText.value);
+    /*Тьфу блин*/
+    /*Эти ваши анимации*/
+    /*Не буду их делать*/
 }
